dcf815c0f86f0afe3a1369753badd133c4ff05a3
Brian C. Van Essen <vanessen1@llnl.gov>
2016-07-18 10:26:02 -0700

    Removed errant build directory and fixed URL to github in license file.

--- build/vandamme.llnl.gov/CMakeFiles/lbann.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "liblbann.pdb"
-  "liblbann.dylib"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/lbann.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/vandamme.llnl.gov/model_zoo/CMakeFiles/lbann_dnn_imagenet.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for lbann_dnn_imagenet.
-# This may be replaced when dependencies are built.
