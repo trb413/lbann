a689df5f40380b54f20aa25feda08cbcd916c001
Travis CI - Documentation Builder <vanessen1@llnl.gov>
2017-05-01 18:49:52 +0000

    Deploy code docs to GitHub Pages Travis build: 135
    Commit: 34e09f4a6b79be9125fd20158af0a44523e6efdc

--- lbann__proto__common_8cpp.js
+++ lbann__proto__common_8cpp.js
@@ -1,8 +1,8 @@
 var lbann__proto__common_8cpp =
 [
     [ "init_data_readers", "lbann__proto__common_8cpp.html#aaac7a1330b8214080e589950fac79b3e", null ],
-    [ "init_model", "lbann__proto__common_8cpp.html#aa0a747b9489931e76c0176945b0c4ffd", null ],
-    [ "init_optimizer_factory", "lbann__proto__common_8cpp.html#ad3bf439c18108f3831e51009cc6a8c99", null ],
+    [ "init_model", "lbann__proto__common_8cpp.html#a65210505c1def636a8c1d230388d2660", null ],
+    [ "init_optimizer_factory", "lbann__proto__common_8cpp.html#a1157a7f2b93a65f5e55e075cacbed91d", null ],
     [ "readPrototextFile", "lbann__proto__common_8cpp.html#a412ce24eaf3f2bff1884da396f2542dc", null ],
     [ "writePrototextFile", "lbann__proto__common_8cpp.html#a88ace24fdc2491da9c6fd95126799a80", null ]
 ];
